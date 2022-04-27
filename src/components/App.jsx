6b99f4e79e7d485673f59097e618984e76a1633e
Anatolii-med <med.anatolii@gmail.com>
2022-04-27 23:30:59 +0300

    add done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,76 +6,84 @@ import Filter from './filter/filter';
 import shortid from 'shortid';
 import { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { AddItem } from 'redux/store';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 const DEFAULT_LIST = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+	{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+	{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+	{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+	{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 function App() {
-    const [contacts, setContacts] = useState(
-        JSON.parse(localStorage.getItem('contacts')) || DEFAULT_LIST
-    );
-    const [filter, setFilter] = useState('');
+	const dispatch = useDispatch();
+	const contactValue = useSelector(state => state.contacts.items);
 
-    useEffect(() => {
-        if (localStorage.getItem('contacts')) {
-            const cont = JSON.parse(localStorage.getItem('contacts'));
-            setContacts(cont);
-        }
-    }, []);
+	const [contacts, setContacts] = useState(
+		JSON.parse(localStorage.getItem('contacts')) || DEFAULT_LIST
+	);
+	const [filter, setFilter] = useState('');
 
-    useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }, [contacts]);
+	// useEffect(() => {
+	// 	if (localStorage.getItem('contacts')) {
+	// 		const cont = JSON.parse(localStorage.getItem('contacts'));
+	// 		dispatch(AddItem(cont));
+	// 	}
+	// }, []);
 
-    const normalizeFilter = filter.toLowerCase();
-    const filterCurrentName = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizeFilter)
-    );
+	useEffect(() => {
+		localStorage.setItem('contacts', JSON.stringify(contactValue));
+	}, [contactValue]);
 
-    function addContactItem(name, number) {
-        const contact = {
-            id: shortid.generate(),
-            name,
-            number,
-        };
-        setContacts([contact, ...contacts]);
-    }
+	const normalizeFilter = filter.toLowerCase();
+	const filterCurrentName = contactValue.filter(contact =>
+		contact.name.toLowerCase().includes(normalizeFilter)
+	);
 
-    const onFilter = e => {
-        setFilter(e.target.value);
-    };
+	function addContactItem(name, number) {
+		const contact = {
+			id: shortid.generate(),
+			name,
+			number,
+		};
+		// setContacts([contact, ...contacts]);
+		dispatch(AddItem(contact));
+		console.log(contactValue);
+	}
 
-    const deleteContacts = id => {
-        setContacts(prevState => contacts.filter(prev => prev.id !== id));
-    };
+	const onFilter = e => {
+		setFilter(e.target.value);
+	};
 
-    return (
-        <>
-            <Section title="Phonebook">
-                <ContactForm
-                    onAdd={addContactItem}
-                    filterContact={filterCurrentName}
-                />
-            </Section>
-            {contacts.length === 0 ? (
-                'No contacts yet'
-            ) : (
-                <Section title="Contacts">
-                    <Filter filterString={filter} onChange={onFilter} />
-                    <List>
-                        <ContactList
-                            contList={filterCurrentName}
-                            deleteCont={deleteContacts}
-                        />
-                    </List>
-                </Section>
-            )}
-            <Toaster />
-        </>
-    );
+	const deleteContacts = id => {
+		setContacts(prevState => contacts.filter(prev => prev.id !== id));
+	};
+
+	return (
+		<>
+			<Section title="Phonebook">
+				<ContactForm
+					onAdd={addContactItem}
+					filterContact={filterCurrentName}
+				/>
+			</Section>
+			{contacts.length === 0 ? (
+				'No contacts yet'
+			) : (
+				<Section title="Contacts">
+					<Filter filterString={filter} onChange={onFilter} />
+					<List>
+						<ContactList
+							contList={filterCurrentName}
+							deleteCont={deleteContacts}
+						/>
+					</List>
+				</Section>
+			)}
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
